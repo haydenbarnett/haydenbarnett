65963f9c83a50d4a9511c82fc7fdf473b7919eb0
Hayden Barnett <hello@haydenbarnett.com>
2021-11-04 23:22:35 +1100

    Add fade in animation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,17 +3,34 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./styles.module.css";
 
-export const Header: FC = () => (
-  <div className={styles.header}>
-    <Link href="/" passHref>
-      <a>
-        <span className={styles.profile}>
-          <Image src="/profile.png" width="60" height="60" alt="" />
-        </span>
-        <span className={styles.logo}>Hayden Barnett</span>
-      </a>
-    </Link>
-  </div>
-);
+type HeaderProps = {
+  onLoadingComplete?: () => void;
+};
+
+export const Header: FC<HeaderProps> = ({ onLoadingComplete }) => {
+  return (
+    <div className={styles.header}>
+      <Link href="/" passHref>
+        <a>
+          <span className={styles.profile}>
+            <Image
+              src="/profile.png"
+              width="60"
+              height="60"
+              alt=""
+              onLoad={(e) => {
+                const target = e.target as HTMLImageElement;
+                if (target.src.indexOf("data:image/gif;base64") < 0) {
+                  onLoadingComplete && onLoadingComplete();
+                }
+              }}
+            />
+          </span>
+          <span className={styles.logo}>Hayden Barnett</span>
+        </a>
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+
 import { Error } from "../components";
+import styles from "../styles/index.module.css";
 
 const Error404: NextPage = () => {
   return (
-    <div className="wrapper">
+    <div className={styles.wrapper}>
       <Error />
     </div>
   );

a8eecffca04b581450910558fb357b6b5b11cd4a
Hayden Barnett <hello@haydenbarnett.com>
2022-10-25 21:08:18 +1100

    Add app types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
 import { useEffect, useRef } from 'react';
+import { AppProps } from 'next/app';
 
 import { Header, Footer } from '@/components';
 
 import '@/styles/tailwind.css';
 import 'focus-visible';
 
-function usePrevious(value) {
-  let ref = useRef();
+const usePrevious = (value: string) => {
+  let ref = useRef<string>();
 
   useEffect(() => {
     ref.current = value;
   }, [value]);
 
   return ref.current;
-}
+};
 
-export default function App({ Component, pageProps, router }) {
+export default function App({ Component, pageProps, router }: AppProps) {
   let previousPathname = usePrevious(router.pathname);
 
   return (

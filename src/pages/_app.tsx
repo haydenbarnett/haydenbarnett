8dde7d16a3df2b9ca46f26f818591426ada34e74
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-22 01:11:36 -0500

    Add offline handler, fix command bar router onchange

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,42 @@
 import type { FC } from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import clsx from 'clsx';
 import { useCommandBar } from '@haydenbleasel/command-bar';
+import useOnline from '@haydenbleasel/use-online';
+import { toast, Toaster } from 'react-hot-toast';
+import Router from 'next/router';
 import { Header, Footer } from '@/components';
 import '@/styles/tailwind.css';
 import 'focus-visible';
 import { inter } from '@/utils/font';
 import ShortcutBar from '@/components/ShortcutBar';
 
-const usePrevious = (value: string) => {
-  const ref = useRef<string>();
-
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-
-  return ref.current;
-};
-
-const App: FC<AppProps> = ({ Component, pageProps, router }) => {
-  const previousPathname = usePrevious(router.pathname);
+const App: FC<AppProps> = ({ Component, pageProps }) => {
   const commandBar = useCommandBar();
+  const isOnline = useOnline();
 
-  router.events.on('routeChangeComplete', () => {
+  Router.events.on('routeChangeStart', () => {
     commandBar.toggleOpen(false);
   });
+
+  useEffect(() => {
+    if (!isOnline) {
+      toast.error(
+        'You are offline, some features may not work as expected until you reconnect.'
+      );
+    }
+  }, [isOnline]);
 
   return (
     <div className="relative">
       <Header />
       <main className={clsx('font-sans', inter.className)}>
-        <Component previousPathname={previousPathname} {...pageProps} />
+        <Component {...pageProps} />
       </main>
       <ShortcutBar />
       <Footer />
+      <Toaster />
     </div>
   );
 };

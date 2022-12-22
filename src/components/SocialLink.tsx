4af29fcb3b077e474a0e796f1fd90824849a057e
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-22 01:19:09 -0500

    Fix social links

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -9,6 +9,8 @@ type SocialLinkProps = {
 export const SocialLink: FC<SocialLinkProps> = ({ icon: Icon, ...props }) => (
   <Link
     className="p-0.5 text-zinc-500 transition hover:text-zinc-800 dark:hover:text-zinc-200"
+    target="_blank"
+    rel="noopener noreferrer"
     {...props}
   >
     <Icon className="h-5 w-5" />

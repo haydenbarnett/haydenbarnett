914eff97e70a3f5483957b5baa21e2d0495756d3
Hayden Barnett <hello@haydenbarnett.com>
2023-01-29 20:15:05 +1100

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 import { Header, Footer } from '@/components';
 
@@ -7,13 +8,16 @@ import '@/styles/tailwind.css';
 import 'focus-visible';
 
 const App: FC<AppProps> = ({ Component, pageProps, router }) => (
-  <div className="relative">
-    <Header />
-    <main>
-      <Component {...pageProps} />
-    </main>
-    {router.pathname !== '/404' && <Footer />}
-  </div>
+  <>
+    <div className="relative">
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      {router.pathname !== '/404' && <Footer />}
+    </div>
+    <Analytics />
+  </>
 );
 
 export default App;

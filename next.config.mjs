46b30c1434941861caba97a966932c8edb5b54f9
Hayden Barnett <hello@haydenbarnett.com>
2023-01-29 17:21:47 +1100

    Add next-secure-headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
 import rehypePrism from '@mapbox/rehype-prism';
+import { createSecureHeaders } from 'next-secure-headers';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,6 +10,21 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     scrollRestoration: true,
+  },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          ...createSecureHeaders(),
+          // HSTS Preload: https://hstspreload.org/
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
   },
 };
 

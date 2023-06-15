4b056417b3664392b412a79235c0789abef74ba4
Hayden Barnett <hello@haydenbarnett.com>
2023-06-15 20:35:58 +1000

    Force dark mode default

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -2,27 +2,9 @@ import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import type { FC } from 'react';
 
 export const ModeToggle: FC = () => {
-  const disableTransitionsTemporarily = () => {
-    document.documentElement.classList.add('[&_*]:!transition-none');
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none');
-    }, 0);
-  };
-
   const toggleMode = () => {
-    disableTransitionsTemporarily();
-
-    const darkModeMediaQuery = window.matchMedia(
-      '(prefers-color-scheme: dark)'
-    );
-    const isSystemDarkMode = darkModeMediaQuery.matches;
     const isDarkMode = document.documentElement.classList.toggle('dark');
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode;
-    } else {
-      window.localStorage.isDarkMode = isDarkMode;
-    }
+    window.localStorage.isDarkMode = isDarkMode;
   };
 
   return (

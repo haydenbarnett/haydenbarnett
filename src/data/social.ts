13774f2055f7a95aae025ce8bad5af6e409bac94
Hayden Barnett <hello@haydenbarnett.com>
2023-06-17 10:48:23 +1000

    Remove dribble and instagram

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,12 +1,7 @@
 import type { FC } from 'react';
 import { EnvelopeIcon } from '@heroicons/react/24/solid';
 
-import {
-  DribbbleIcon,
-  GitHubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-} from '../components/SocialIcons';
+import { GitHubIcon, LinkedInIcon } from '../components/SocialIcons';
 
 export type SocialDataProps = {
   href: string;
@@ -21,19 +16,9 @@ export const social: SocialDataProps[] = [
     icon: GitHubIcon,
   },
   {
-    href: 'https://dribbble.com/haydenbarnett',
-    label: 'Dribbble',
-    icon: DribbbleIcon,
-  },
-  {
     href: 'https://www.linkedin.com/in/haydenbarnett/',
     label: 'LinkedIn',
     icon: LinkedInIcon,
-  },
-  {
-    href: 'https://www.instagram.com/hayden_jb/',
-    label: 'Instagram',
-    icon: InstagramIcon,
   },
   {
     href: 'mailto:hello@haydenbarnett.com',

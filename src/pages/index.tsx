54489a09bc9b571ebb4d398e0e51b10d73d7a108
Hayden Barnett <hello@haydenbarnett.com>
2023-06-23 21:51:14 +1000

    Fade in ascii

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+import { motion } from 'framer-motion';
 import { Seo } from '@/components';
 import type { FC } from 'react';
 
@@ -20,7 +22,19 @@ const Home: FC = () => (
   <>
     <Seo />
     <div className="hidden lg:block">
-      <Ascii />
+      <Suspense fallback={null}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{
+            opacity: 0,
+            transition: { delay: 0 },
+          }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <Ascii />
+        </motion.div>
+      </Suspense>
     </div>
   </>
 );

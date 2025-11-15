ce4fadcb23dbdea80eca3a66780a57a7b3054abb
Hayden Barnett <hello@haydenbarnett.com>
2025-11-15 17:06:45 +1100

    Content cleanup

--- src/components/projects-list.tsx
+++ src/components/projects-list.tsx
@@ -2,26 +2,20 @@ import { projects } from '@/data/projects';
 import type { FC } from 'react';
 import { Row } from './row';
 
-type ProjectsListProps = {
-  readonly studioOnly?: boolean;
-};
-
-export const ProjectsList: FC<ProjectsListProps> = ({ studioOnly }) => (
+export const ProjectsList: FC = () => (
   <div className="flex flex-col">
-    {projects
-      .filter((project) => (studioOnly ? project.studio : true))
-      .map((project) => {
-        const { name, link, date } = project;
-        return (
-          <Row
-            key={name}
-            href={link?.href}
-            hrefLabel={link?.label}
-            title={name}
-            date={date}
-            isExternal
-          />
-        );
-      })}
+    {projects.map((project) => {
+      const { name, link, date } = project;
+      return (
+        <Row
+          key={name}
+          href={link?.href}
+          hrefLabel={link?.label}
+          title={name}
+          date={date}
+          isExternal
+        />
+      );
+    })}
   </div>
 );
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,6 @@ type ProjectDataProps = {
     href: string;
     label: string;
   };
-  studio?: boolean;
 };
 
 export const projects: ProjectDataProps[] = [
@@ -18,66 +17,36 @@ export const projects: ProjectDataProps[] = [
       href: 'https://elements.surge.studio',
       label: 'elements.surge.studio',
     },
-    studio: true,
   },
   {
     date: '2023',
     name: 'Theme.ink',
     description: 'Development tools and resources for the modern web.',
     link: { href: 'https://theme.ink', label: 'theme.ink' },
-    studio: true,
   },
   {
     date: '2023',
     name: 'Spark',
     description: 'An open source design system for all studio projects.',
     link: { href: 'https://spark.surge.studio', label: 'spark.surge.studio' },
-    studio: true,
   },
   {
     date: '2023',
     name: 'Eval AI',
     description: 'Say hello to Eval, your new AI companion.',
     link: { href: 'https://eval.surge.studio', label: 'eval.surge.studio' },
-    studio: true,
   },
   {
     date: '2023',
     name: 'Imagepup',
     description: 'Playground for AI generated imagery and associated tools.',
     link: { href: 'https://www.imagepup.com', label: 'imagepup.com' },
-    studio: true,
   },
   {
     date: '2022',
     name: 'Temploid',
     description: 'Play games for free online.',
     link: { href: 'https://www.temploid.com', label: 'temploid.com' },
-    studio: true,
-  },
-  {
-    date: '2022',
-    name: 'Anna & Hayden',
-    description:
-      'A simple website to share our photos and videos from our wedding event.',
-    link: {
-      href: 'https://wedding.haydenbarnett.com',
-      label: 'wedding.haydenbarnett.com',
-    },
-  },
-  {
-    date: '2021',
-    name: 'Anna Ji',
-    description:
-      'Short, sweet and straight to the point. Am I talking about the website or my wife? :)',
-    link: { href: 'https://annaphase.com', label: 'annaphase.com' },
-  },
-  {
-    date: '2019',
-    name: 'Ben Wotton',
-    description:
-      'Portfolio website for Ben, a Senior Digital Compositor working in the film industry.',
-    link: { href: 'https://benwotton.com', label: 'benwotton.com' },
   },
   {
     date: '2016',
@@ -87,26 +56,6 @@ export const projects: ProjectDataProps[] = [
     link: {
       href: 'https://www.freegameassets.com',
       label: 'freegameassets.com',
-    },
-  },
-  {
-    date: '2011',
-    name: 'Stormhyde',
-    description:
-      'Free software and resources created for animators for use with Autodesk Maya.',
-    link: {
-      href: 'https://www.stormhyde.com',
-      label: 'stormhyde.com',
-    },
-  },
-  {
-    date: '2011',
-    name: 'Barnetts Deniliquin',
-    description:
-      'The long standing family company with a history of sound and careful planning, expansion and contract delivery.',
-    link: {
-      href: 'https://barnettsdeniliquin.com',
-      label: 'barnettsdeniliquin.com',
     },
   },
 ];
